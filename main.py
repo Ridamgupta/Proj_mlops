6c615357b4ee4e579736d5cc883cff68bcf9fa1b
Ridam Gupta <104208414+Ridamgupta@users.noreply.github.com>
2024-04-05 19:49:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,134 +1,43 @@
-import subprocess
-import sys
-import os
-import numpy as np
-import torch
-from evaluation import seg_acc, cls_acc
-from data_handling import create_loaders_cls, create_loaders_seg
-from modules import UnetDown, Unet
-subprocess.check_call([sys.executable, "-m", "pip", "install", "milankalkenings==0.0.9"])
-from milankalkenings.deep_learning import Module, TrainerSetup, Trainer, make_reproducible
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
 
-make_reproducible()
-os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+with open('/content/Emocontext.txt', 'r') as file:
+    lines = file.readlines()
 
+data = []
+for line in lines[1:]: 
+    parts = line.strip().split('\t')
+    text = ' '.join(parts[1:4])  
+    label = parts[-1]
+    data.append((text, label))
 
-# cls setup
-loader_train_cls, loader_val_cls = create_loaders_cls(batch_size=32)
-print("cls train batches:", len(loader_train_cls), "cls val batches:", len(loader_val_cls))
-trainer_setup_cls = TrainerSetup()
-trainer_setup_cls.checkpoint_initial = "../monitoring/checkpoint_initial_cls.pkl"
-trainer_setup_cls.checkpoint_running = "../monitoring/checkpoint_running_cls.pkl"
-trainer_setup_cls.lrrt_n_batches = 10  # lrrt on about 10% of an epoch
-trainer_setup_cls.monitor_n_losses = 50
-trainer_setup_cls.lrrt_max_decays = 3
-train_epochs_max_cls = 50
+X = [sample[0] for sample in data]
+y = [sample[1] for sample in data]
 
-# seg setup
-loader_train_seg, loader_val_seg, class_weights_seg = create_loaders_seg(batch_size=4)
-print("seg train batches:", len(loader_train_seg), "seg val batches:", len(loader_val_seg))
-trainer_setup_seg = TrainerSetup()
-trainer_setup_seg.checkpoint_initial = "../monitoring/checkpoint_initial_seg.pkl"
-trainer_setup_seg.checkpoint_running = "../monitoring/checkpoint_running_seg.pkl"
-trainer_setup_seg.lrrt_n_batches = 10  # lrrt on a whole epoch
-trainer_setup_seg.monitor_n_losses = 11  # 11 > 10, not shown
-trainer_setup_seg.lrrt_max_decays = 1
-trainer_setup_seg.lrrt_initial_candidates = np.array([5e-1, 1e-1, 1e-2, 1e-3, 1e-4, 1e-6])
-train_epochs_max_seg = 1000
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# stage 1: cls pretraining
-
-# save the initial checkpoint once
-unet_down = UnetDown()
-torch.save(unet_down, trainer_setup_cls.checkpoint_initial)
+pipeline = Pipeline([
+    ('tfidf', TfidfVectorizer()),
+    ('classifier', MultinomialNB())
+])
 
 
-unet_down = torch.load(trainer_setup_cls.checkpoint_initial)
-torch.save(unet_down, trainer_setup_cls.checkpoint_running)
-cls_acc_train_prior = cls_acc(module=unet_down, loader_eval=loader_train_cls)
-cls_acc_val_prior = cls_acc(module=unet_down, loader_eval=loader_val_cls)
-print("initial cls acc train", cls_acc_train_prior, "initial cls acc val", cls_acc_val_prior)
+pipeline.fit(X_train, y_train)
 
-trainer_cls = Trainer(loader_train=loader_train_cls, loader_val=loader_val_cls, setup=trainer_setup_cls)
-trainer_cls.train_n_epochs_early_stop_initial_lrrt(max_epochs=train_epochs_max_cls, freeze_pretrained_layers=False)
+def predict_emotion(text):
+    prediction = pipeline.predict([text])
+    return prediction[0]
 
-unet_down = torch.load(trainer_setup_cls.checkpoint_running)
-cls_acc_train_post = cls_acc(module=unet_down, loader_eval=loader_train_cls)
-cls_acc_val_post = cls_acc(module=unet_down, loader_eval=loader_val_cls)
-print("trained cls acc train", cls_acc_train_post, "trained cls acc val", cls_acc_val_post)
-
-pretrained_unet_down = torch.load(trainer_setup_cls.checkpoint_running)
-torch.save(pretrained_unet_down, "../monitoring/checkpoint_pretrained_undet_down.pkl")
-
-# seg fine tuning
-
-# save the initial checkpoint once and replace its unet_down with cls pretrained component
-unet = Unet(loss_class_weights=class_weights_seg)
-unet.unet_down = torch.load("../monitoring/checkpoint_pretrained_undet_down.pkl")
-torch.save(unet, trainer_setup_seg.checkpoint_initial)
-
-print("\n\nseg fine tuning")
-unet = torch.load(trainer_setup_seg.checkpoint_initial)
-torch.save(unet, trainer_setup_seg.checkpoint_running)
-seg_acc_train_initial = seg_acc(module=unet, loader_eval=loader_train_seg)
-seg_acc_val_initial = seg_acc(module=unet, loader_eval=loader_val_seg)
-print("initial seg acc train", seg_acc_train_initial, "initial seg acc val", seg_acc_val_initial)
-
-print("adjusting the upwards part to the pretrained downwards part")
-trainer_seg = Trainer(loader_train=loader_train_seg, loader_val=loader_val_seg, setup=trainer_setup_seg)
-trainer_seg.train_n_epochs_early_stop_initial_lrrt(max_epochs=train_epochs_max_seg, freeze_pretrained_layers=True)
-
-unet = torch.load(trainer_setup_seg.checkpoint_running)
-seg_acc_train_adjusted = seg_acc(module=unet, loader_eval=loader_train_seg)
-seg_acc_val_adjusted = seg_acc(module=unet, loader_eval=loader_val_seg)
-print("adjusted seg acc train", seg_acc_train_adjusted, "adjusted seg acc val", seg_acc_val_adjusted)
-
-print("\n\ntraining the whole model")
-trainer_seg = Trainer(loader_train=loader_train_seg, loader_val=loader_val_seg, setup=trainer_setup_seg)
-trainer_seg.train_n_epochs_early_stop_initial_lrrt(max_epochs=train_epochs_max_seg, freeze_pretrained_layers=False)
-
-unet = torch.load(trainer_setup_seg.checkpoint_running)
-seg_acc_train_final = seg_acc(module=unet, loader_eval=loader_train_seg)
-seg_acc_val_final = seg_acc(module=unet, loader_eval=loader_val_seg)
-print("final seg acc train", seg_acc_train_final, "final seg acc val", seg_acc_val_final)
-torch.save(unet, "../monitoring/checkpoint_final.pkl")
-
-
-print("alternatively without pretraining")
-unet = Unet(loss_class_weights=class_weights_seg)  # random init, not from checkpoint with pretrained weights
-torch.save(unet, trainer_setup_seg.checkpoint_initial)  # overwrite initial checkpoint with random init unet
-torch.save(unet, trainer_setup_seg.checkpoint_running)  # overwrite running checkpoint with random init unet
-
-
-print("\n\ntraining from scratch")
-trainer_seg = Trainer(loader_train=loader_train_seg, loader_val=loader_val_seg, setup=trainer_setup_seg)
-trainer_seg.train_n_epochs_early_stop_initial_lrrt(max_epochs=train_epochs_max_seg, freeze_pretrained_layers=False)
-
-unet = torch.load(trainer_setup_seg.checkpoint_running)
-seg_acc_train_final = seg_acc(module=unet, loader_eval=loader_train_seg)
-seg_acc_val_final = seg_acc(module=unet, loader_eval=loader_val_seg)
-print("seg acc train after training from scratch", seg_acc_train_final, "seg acc val after training from scratch", seg_acc_val_final)
-torch.save(unet, "../monitoring/checkpoint_alt_final.pkl")
-
-
-# final evaluation
-loader_train_seg, loader_val_seg, _ = create_loaders_seg(batch_size=4)
-unet_with_pretrain = torch.load("../monitoring/checkpoint_final.pkl")
-unet_without_pretrain = torch.load("../monitoring/checkpoint_alt_final.pkl")
-
-for p1, p2 in zip(unet_without_pretrain.parameters(), unet_with_pretrain.parameters()):
-    if (p1 != p2).sum().item() != 0:
-        print("Models have different parameters")
-    else:
-        print("Models have the same parameters")
-
-acc_train_without_pretrain = seg_acc(module=unet_without_pretrain, loader_eval=loader_train_seg)
-acc_val_without_pretrain = seg_acc(module=unet_without_pretrain, loader_eval=loader_val_seg)
-
-acc_train_with_pretrain = seg_acc(module=unet_with_pretrain, loader_eval=loader_train_seg)
-acc_val_with_pretrain = seg_acc(module=unet_with_pretrain, loader_eval=loader_val_seg)
-
-print("acc_train_without_pretrain", acc_train_without_pretrain)
-print("acc_val_without_pretrain", acc_val_without_pretrain)
-print("acc_train_with_pretrain", acc_train_without_pretrain)
-print("acc_val_with_pretrain", acc_val_without_pretrain)
+while True:
+    try:
+        input_text = input("Enter text (press 'q' to quit): ")
+        if input_text.lower() == 'q':
+            break
+        else:
+            prediction = predict_emotion(input_text)
+            print("Predicted emotion:", prediction)
+    except KeyboardInterrupt:
+        print("\nExiting...")
+        break
